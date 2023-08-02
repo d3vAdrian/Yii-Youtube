a85fcb9d2ac245ccf8784aa4e6013f985492e633
d3vAdrian <d3vs3c0ps.adrian@gmail.com>
2023-08-02 00:52:53 -0700

    bare bone skeleton complete --success

---
+++ backend/controllers/helloController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace backend\controllers;
+
+class HelloController extends Controller //hello
+{
+    public function actionIndex() //index
+    {
+        return $this->render(v);
+    }
+}
+?>
